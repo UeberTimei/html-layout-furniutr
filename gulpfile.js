7377f4b6acfb73e60deebb53d7f3feed4c3ca629
UeberTimei <super.timati04@gmail.com>
2024-01-30 19:00:32 +0300

    Make styles for the footer.
    Installed gulp-concat for concating all css files into one file.
    
    Made mixins.scss file for frequently using mixins.
    Made styles for the footer section in scss, also using new mixins.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const concat = require("gulp-concat");
 
 const buildStyles = () => {
-  return src("./scss/**/*.scss").pipe(sass()).pipe(dest("./css"));
+  return src("./scss/**/*.scss")
+    .pipe(sass())
+    .pipe(concat("index.css"))
+    .pipe(dest("./css"));
 };
 
 const watchTask = () => {
